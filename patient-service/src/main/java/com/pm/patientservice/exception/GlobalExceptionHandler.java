7c3546feb173a96c6a99fbc34fa771a3e756d630
Nikhil <nikhilchanda.nc@gmail.com>
2026-04-17 20:10:32 +0530

    updated the global exception handler

--- patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.pm.patientservice.exception;
 
 import java.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     //gets triggered when the input validation is done by jpa on the requestdtos
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex){
@@ -20,4 +24,22 @@ public class GlobalExceptionHandler {
     }
 
 
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String,String>> handleEmailLAlreadyExistsException(EmailAlreadyExistsException ex){
+      log.warn("email address already exixts {}" , ex.getMessage());
+        Map<String,String> errors=new HashMap<>();
+        errors.put("message" , "Email address already exists");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(
+            PatientNotFoundException ex) {
+        log.warn("Patient not found {}", ex.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Patient not found");
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
